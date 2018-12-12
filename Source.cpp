727f3578d9f17c17148dc56ee4348702358a8bff
Dakota Wilson <dtw3200@live.com>
2018-12-12 15:15:20 -0600

    V1.0
    Updated so that there are more enemies that spawn as score increases and their movement is more sporadic then the previous one's game is fairly difficult now.

--- Source.cpp
+++ Source.cpp
@@ -5,13 +5,25 @@ using namespace std;
 int main()
 {
 	char GameBoard[10][10], User;
-	int row, col, RandomPointX, RandomPointY, StartPointX, StartPointY, Score, EnemyX, EnemyY, Enemy, Life;
-	bool Quit;
+	int row, col, RandomPointX, RandomPointY, StartPointX, StartPointY, Score, EnemyX, EnemyY, Enemy, Life, Enemy2X, Enemy2Y, Enemy3X, Enemy3Y, Enemy4X, Enemy4Y, Enemy5X, Enemy5Y;
+	bool Quit, Enemy2, Enemy3, Enemy4, Enemy5;
 
 	Quit = false;
+	Enemy2 = false;
+	Enemy3 = false;
+	Enemy4 = false;
+	Enemy5 = false;
 
 	Score = 0;
 	Life = 3;
+	Enemy2X = -10;
+	Enemy3X = -10;
+	Enemy4X = -10;
+	Enemy5X = -10;
+	Enemy2Y = -10;
+	Enemy3Y = -10;
+	Enemy4Y = -10;
+	Enemy5Y = -10;
 
 	srand(time(0));
 
@@ -20,7 +32,7 @@ int main()
 
 	EnemyX = (rand() % 9) + 1;
 	EnemyY = (rand() % 9) + 1;
-	
+
 	for (row = 0; row < 10; row++)
 	{
 		for (col = 0; col < 10; col++)
@@ -39,7 +51,7 @@ int main()
 	}
 
 	if (RandomPointY < 5)
-		
+
 	{
 		StartPointY = RandomPointY + 6;
 	}
@@ -50,28 +62,45 @@ int main()
 
 	cout << "Created by : Dakota Wilson!\n\nScore points by getting yourself (X) to the doctor (Y) but dont get touched\nby the killer (K) or let him get to the doctor first or you will lose a life!\n\n";
 
-	while (!Quit && Life > 0) 
+	while (!Quit && Life > 0)
 	{
 		cout << "w = X up, a = X left, s = X down, d = right, p = quit\n\n";
 		cout << "Score: " << Score << '\n';
 		cout << "Lives: " << Life << '\n';
-		
+
 		for (row = 0; row < 10; row++)
 		{
-			
+
 			for (col = 0; col < 10; col++)
 			{
-				
+
 				cout << GameBoard[row][col] << "  ";
 				GameBoard[RandomPointX][RandomPointY] = 'Y';
 				GameBoard[StartPointX][StartPointY] = 'X';
 				GameBoard[EnemyX][EnemyY] = 'K';
+				if (Score >= 1)
+				{
+					GameBoard[Enemy2X][Enemy2Y] = 'K';
+				}
+				if (Score >= 2)
+				{
+					GameBoard[Enemy3X][Enemy3Y] = 'K';
+				}
+				if (Score >= 3)
+				{
+					GameBoard[Enemy4X][Enemy4Y] = 'K';
+				}
+				if (Score >= 4)
+				{
+					GameBoard[Enemy5X][Enemy5Y] = 'K';
+				}
+
 			}
 			cout << '\n';
 		}
-		
+
 		cin >> User;
-		
+
 		switch (User)
 		{
 		case 'w':
@@ -93,7 +122,7 @@ int main()
 		case 'p':
 			Quit = true;
 			break;
-		default :
+		default:
 			cout << "Invalid Input! Try Again!\n";
 			cin >> User;
 		}
@@ -105,23 +134,101 @@ int main()
 		case 1:
 			GameBoard[EnemyX][EnemyY] = '-';
 			EnemyX -= 1;
+			if (Score >= 1)
+			{
+				GameBoard[Enemy2X][Enemy2Y] = '-';
+				Enemy2X += 1;
+			}
+			if (Score >= 2)
+			{
+				GameBoard[Enemy3X][Enemy3Y] = '-';
+				Enemy3X -= 2;
+			}
+			if (Score >= 3)
+			{
+				GameBoard[Enemy4X][Enemy4Y] = '-';
+				Enemy4X += 2;
+			}
+			if (Score >= 4)
+			{
+				GameBoard[Enemy5X][Enemy5Y] = '-';
+				Enemy5X -= 3;
+			}
 			break;
 		case 2:
 			GameBoard[EnemyX][EnemyY] = '-';
 			EnemyY -= 1;
+			if (Score >= 1)
+			{
+				GameBoard[Enemy2X][Enemy2Y] = '-';
+				Enemy2Y += 1;
+			}
+			if (Score >= 2)
+			{
+				GameBoard[Enemy3X][Enemy3Y] = '-';
+				Enemy3Y -= 2;
+			}
+			if (Score >= 3)
+			{
+				GameBoard[Enemy4X][Enemy4Y] = '-';
+				Enemy4Y += 2;
+			}
+			if (Score >= 4)
+			{
+				GameBoard[Enemy5X][Enemy5Y] = '-';
+				Enemy5Y -= 3;
+			}
 			break;
 		case 3:
 			GameBoard[EnemyX][EnemyY] = '-';
 			EnemyX += 1;
+			if (Score >= 1)
+			{
+				GameBoard[Enemy2X][Enemy2Y] = '-';
+				Enemy2X -= 1;
+			}
+			if (Score >= 2)
+			{
+				GameBoard[Enemy3X][Enemy3Y] = '-';
+				Enemy3X += 2;
+			}
+			if (Score >= 3)
+			{
+				GameBoard[Enemy4X][Enemy4Y] = '-';
+				Enemy4X -= 2;
+			}
+			if (Score >= 4)
+			{
+				GameBoard[Enemy5X][Enemy5Y] = '-';
+				Enemy5X += 3;
+			}
 			break;
 		case 4:
 			GameBoard[EnemyX][EnemyY] = '-';
 			EnemyY += 1;
+			if (Score >= 1)
+			{
+				GameBoard[Enemy2X][Enemy2Y] = '-';
+				Enemy2Y -= 1;
+			}
+			if (Score >= 2)
+			{
+				GameBoard[Enemy3X][Enemy3Y] = '-';
+				Enemy3Y += 2;
+			}
+			if (Score >= 3)
+			{
+				GameBoard[Enemy4X][Enemy4Y] = '-';
+				Enemy4Y -= 2;
+			}
+			if (Score >= 4)
+			{
+				GameBoard[Enemy5X][Enemy5Y] = '-';
+				Enemy5Y += 3;
+			}
 			break;
 		}
 
-
-		
 		if (StartPointX == RandomPointX && StartPointY == RandomPointY)
 		{
 			Score += 1;
@@ -138,7 +245,7 @@ int main()
 		{
 			StartPointX -= 10;
 		}
-		
+
 		if (StartPointY == -1)
 		{
 			StartPointY += 10;
@@ -166,11 +273,133 @@ int main()
 			EnemyY -= 10;
 		}
 
-		if ((EnemyX == RandomPointX && EnemyY == RandomPointY) || (EnemyX == StartPointX && EnemyY == StartPointY))
+		if (Enemy2X == -1)
+		{
+			Enemy2X += 10;
+		}
+		else if (Enemy2X == 10)
+		{
+			Enemy2X -= 10;
+		}
+
+		if (Enemy2Y == -1)
+		{
+			Enemy2Y += 10;
+		}
+		else if (Enemy2Y == 10)
+		{
+			Enemy2Y -= 10;
+		}
+
+		if (Enemy3X <= -1)
+		{
+			Enemy3X += 10;
+		}
+		else if (Enemy3X >= 10)
+		{
+			Enemy3X -= 10;
+		}
+
+		if (Enemy3Y <= -1)
+		{
+			Enemy3Y += 10;
+		}
+		else if (Enemy3Y >= 10)
+		{
+			Enemy3Y -= 10;
+		}
+
+		if (Enemy4X <= -1)
+		{
+			Enemy4X += 10;
+		}
+		else if (Enemy4X >= 10)
+		{
+			Enemy4X -= 10;
+		}
+
+		if (Enemy4Y <= -1)
+		{
+			Enemy4Y += 10;
+		}
+		else if (Enemy4Y >= 10)
+		{
+			Enemy4Y -= 10;
+		}
+
+		if (Enemy5X <= -1)
+		{
+			Enemy5X += 10;
+		}
+		else if (Enemy5X >= 10)
+		{
+			Enemy5X -= 10;
+		}
+
+		if (Enemy5Y <= -1)
+		{
+			Enemy5Y += 10;
+		}
+		else if (Enemy5Y >= 10)
+		{
+			Enemy5Y -= 10;
+		}
+
+		if ((EnemyX == RandomPointX && EnemyY == RandomPointY) || (EnemyX == StartPointX && EnemyY == StartPointY) ||
+			(Enemy2X == RandomPointX && Enemy2Y == RandomPointY) || (Enemy2X == StartPointX && Enemy2Y == StartPointY) ||
+			(Enemy3X == RandomPointX && Enemy3Y == RandomPointY) || (Enemy3X == StartPointX && Enemy3Y == StartPointY) ||
+			(Enemy4X == RandomPointX && Enemy4Y == RandomPointY) || (Enemy4X == StartPointX && Enemy4Y == StartPointY) ||
+			(Enemy5X == RandomPointX && Enemy5Y == RandomPointY) || (Enemy5X == StartPointX && Enemy5Y == StartPointY))
 		{
 			Life -= 1;
 		}
+
+		if (Score == 1)
+		{
+			Enemy2 = true;
+		}
+		if (Score == 2)
+		{
+			Enemy3 = true;
+		}
+		if (Score == 3)
+		{
+			Enemy4 = true;
+		}
+		if (Score == 4)
+		{
+			Enemy5 = true;
+		}
+
+		if (Enemy2)
+		{
+			Enemy2X = (rand() % 9) + 1;
+			Enemy2Y = (rand() % 9) + 1;
+			Enemy2 = false;
+		}
+		if (Enemy3)
+		{
+			Enemy3X = (rand() % 9) + 1;
+			Enemy3Y = (rand() % 9) + 1;
+			Enemy3 = false;
+		}
+		if (Enemy4)
+		{
+			Enemy4X = (rand() % 9) + 1;
+			Enemy4Y = (rand() % 9) + 1;
+			Enemy4 = false;
+		}
+		if (Enemy5)
+		{
+			Enemy5X = (rand() % 9) + 1;
+			Enemy5Y = (rand() % 9) + 1;
+			Enemy5 = false;
+		}
+
 	}
 
+	cout << "\n\n\n\n\n\n\nFinal Score: " << Score << "\n\n\n\n\nGood Job!\n\n\n\n\nPress any key to exit!\n\n\n\n\n\n\n\n\n\n\n\n";
+	
+	system("pause");
 	return 0;
 }
